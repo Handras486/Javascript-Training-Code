248465dc0f6d03b18495fe440a352ec5563d5a5c
András <hajduandras486@gmail.com>
2023-10-18 21:13:47 +0200

    Added DOM practice code

---
+++ DOM projects/practice code/app.js
@@ -0,0 +1,7 @@
+const h1 = document.getElementById("title");
+h1.style.color = "red";
+
+document.getElementById("btn").style.backgroundColor = "blue";
+
+const item = document.querySelector(".btn");
+console.log(item.childNodes[0].nodeValue);
