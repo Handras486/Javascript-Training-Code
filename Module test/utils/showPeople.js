1acff5b23957ed4ae7b3481ad8d0d6b62b0b9dea
Andras <ares10486@gmail.com>
2024-05-04 22:10:47 +0200

    Formatted Module practice code

--- Module test/utils/showPeople.js
+++ Module test/utils/showPeople.js
@@ -1,9 +1,11 @@
-const showPeople = (people) =>{
-    const newpeople = people.map((person) =>{
-        const {name, job } = person;
-        return `<p>${name} <strong>${job}</strong></p>`
-    }).join('');
-    return newpeople
+const showPeople = (people) => {
+  const newpeople = people
+    .map((person) => {
+      const { name, job } = person;
+      return `<p>${name} <strong>${job}</strong></p>`;
+    })
+    .join('');
+  return newpeople;
 };
 
-export default showPeople
+export default showPeople;
