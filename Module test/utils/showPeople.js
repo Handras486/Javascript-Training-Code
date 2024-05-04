0cc9a35240935a3afd241dff569226b4b15da12c
Andras <ares10486@gmail.com>
2024-05-04 19:50:26 +0200

    Added Module practice code

---
+++ Module test/utils/showPeople.js
@@ -0,0 +1,9 @@
+const showPeople = (people) =>{
+    const newpeople = people.map((person) =>{
+        const {name, job } = person;
+        return `<p>${name} <strong>${job}</strong></p>`
+    }).join('');
+    return newpeople
+};
+
+export default showPeople
